24f970aaa63eb436cf02aec08d4e775be1d6baa4
Evgeniy <evgenij.arhipov@gmail.com>
2017-09-02 00:45:06 +0300

    fix

--- src/main/java/Commands/ViewAuctionCommand.java
+++ src/main/java/Commands/ViewAuctionCommand.java
@@ -4,8 +4,9 @@ import Utils.AuctionUtil;
 import Utils.Sender;
 
 public class ViewAuctionCommand {
-    public static void execute(Long chatId, String auctionId) {
-        String responseMessage = AuctionUtil.getAuctionRecordsFromDatabase().get(auctionId).toString();
+    public static void execute(Long chatId, String auctionName) {
+        String responseMessage = AuctionUtil.getAuctionRecordsFromDatabase().get(auctionName).toString();
+        responseMessage += "hui";
         Sender.getInstance().sendTextMessage(responseMessage,chatId);
     }
 }
