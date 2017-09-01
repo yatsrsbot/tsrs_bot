f5601e1cd92f0d990fccccc89f412e06240c4ccb
Evgeniy <evgenij.arhipov@gmail.com>
2017-09-02 01:22:08 +0300

    fix

--- src/main/java/Commands/StartCommand.java
+++ src/main/java/Commands/StartCommand.java
@@ -7,6 +7,6 @@ public class StartCommand {
 
 
     public static void execute(Long chatId) {
-        Sender.getInstance().sendTextMessage("Oh, Hi there!",chatId);
+        Sender.getInstance().sendTextMessage("Oh, Hi there!\nYou can work with auction records by sending /auction command",chatId);
     }
 }
