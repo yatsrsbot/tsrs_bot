e183be9a47a01e0a8dd21899ad501694af200e18
Evgeniy <evgenij.arhipov@gmail.com>
2017-09-02 03:01:39 +0300

    fix

--- src/main/java/Commands/ViewAuctionCommand.java
+++ src/main/java/Commands/ViewAuctionCommand.java
@@ -5,9 +5,11 @@ import Utils.Sender;
 
 public class ViewAuctionCommand {
     public static void execute(Long chatId, String auctionName) {
-        String responseMessage = AuctionUtil.getAuctionRecordsFromDatabase().get(auctionName).toString();
-        if (!"".equals(responseMessage)){
-        Sender.getInstance().sendTextMessage(responseMessage,chatId);}
-        else {Sender.getInstance().sendTextMessage("there are no such record",chatId);}
+        if (AuctionUtil.getAuctionRecordsFromDatabase().containsKey(auctionName)) {
+            String responseMessage = AuctionUtil.getAuctionRecordsFromDatabase().get(auctionName).toString();
+            Sender.getInstance().sendTextMessage(responseMessage, chatId);
+        } else {
+            Sender.getInstance().sendTextMessage("there are no such record", chatId);
+        }
     }
 }
